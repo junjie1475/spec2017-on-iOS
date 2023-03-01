b085488704b24c0050735e54972ca85a640658b3
junjie1475 <junjie1475@gmail.com>
2023-03-01 19:04:57 +0800

    Initial Commit

---
+++ Spec2017/Spec2017App.swift
@@ -0,0 +1,17 @@
+//
+//  Spec2017App.swift
+//  Spec2017
+//
+//  Created by Junjie on 01/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct Spec2017App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
